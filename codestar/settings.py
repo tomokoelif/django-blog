e8aacc8775e20700fb9cc5f906e562fe5086b43a
Tomoko Koide <tomokoelif@gmail.com>
2025-01-12 00:04:03 +0000

    Update settings.py to import env module conditionally and set DEBUG to False

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
-from pathlib import Path
 import dj_database_url
+if os.path. isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-nielmc-django-project-0kylrta3cs.us2.codeanyapp.com','.herokuapp.com']
 
@@ -83,7 +84,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #   }
 #}
 
-DATABASE = {
+DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
