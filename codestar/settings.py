65b6682cd9ae0f056f4e010ee8edbd61f4489e41
Tomoko Koide <tomokoelif@gmail.com>
2025-01-12 04:23:22 +0000

    Update ALLOWED_HOSTS in settings.py to include localhost

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-nielmc-django-project-0kylrta3cs.us2.codeanyapp.com','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-nielmc-django-project-0kylrta3cs.us2.codeanyapp.com','127.0.0.1','.herokuapp.com']
 
 
 # Application definition
