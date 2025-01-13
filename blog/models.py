d1e77805b06004e7ed233cb74520b389edc941cd
Tomoko Koide <tomokoelif@gmail.com>
2025-01-13 12:39:39 +0000

    Add Comment model and update Post model; include new fields and migrations

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
 
-
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -16,13 +15,25 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    field_2 = models.IntegerField(default=42)
+    field_3 = models.CharField(max_length=100, null=True, blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments_author")
+    name = models.CharField(max_length=80, default="Anonymous")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    
+
+    class Meta:
+        ordering = ["created_on"]
+
     def __str__(self):
-        return self.title
+        return f"Comment {self.body} by {self.author}"
