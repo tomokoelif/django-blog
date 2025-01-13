3a08c33d1db0cea5b908725c6b4427e7343745ce
Tomoko Koide <tomokoelif@gmail.com>
2025-01-13 21:39:37 +0000

    Update PostList view to filter published posts and set template name; change DEBUG to False for production readiness

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,5 @@ from django.views import generic
 from .models import Post
 
 class PostList(generic.ListView):
-    model = Post
+    queryset = Post.objects.filter(status=1)
+    template_name = "post_list.html"
--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-nielmc-django-project-0kylrta3cs.us2.codeanyapp.com','127.0.0.1',
