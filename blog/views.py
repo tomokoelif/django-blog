ec0831f154862991faddcff282fd93f9171d2637
Tomoko Koide <tomokoelif@gmail.com>
2025-01-14 15:09:24 +0000

    Refactor admin Post list display and search fields; update migrations to include 'updated_on' field in Post model and rename 'active' to 'approved' in Comment model; clean up obsolete migration files; adjust PostList view to use model attribute.

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
+# Create your views here.
+
+
 class PostList(generic.ListView):
+    model = Post
     queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
