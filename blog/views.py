e4ac6c9e8f198116c85fd4b596456507a14ceca5
Tomoko Koide <tomokoelif@gmail.com>
2025-01-14 19:24:13 +0000

    Replace post_list template with index template; update PostList view to set template name and enable pagination; add static files directory to settings

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,5 @@ from .models import Post
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
