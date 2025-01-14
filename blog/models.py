eeedcfeb5cc9d3d82c0b2c5bfe3178ff9eaefeb7
Tomoko Koide <tomokoelif@gmail.com>
2025-01-14 00:45:25 +0000

    Enhance admin interface by adding created_on to Post list display and expanding search fields; update Comment model with email and name fields; modify .gitignore to include blog fixtures; adjust DEBUG setting for development.

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,9 @@
-# filepath: /workspace/django-blog/blog/models.py
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -16,8 +16,6 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-    field_2 = models.IntegerField(default=42)
-    field_3 = models.CharField(max_length=100, null=True, blank=True)
 
     class Meta:
         ordering = ["-created_on"]
@@ -25,17 +23,18 @@ class Post(models.Model):
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
+
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments_author")
-    name = models.CharField(max_length=80, default="Anonymous")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
     body = models.TextField()
-    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    challenge = models.SlugField(default=3.0)
+    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.author}"
+        return f"Comment {self.body} by {self.name}"
--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-nielmc-django-project-0kylrta3cs.us2.codeanyapp.com','127.0.0.1',
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-summernote',
+    'django_summernote',  
     'blog',
 ]
 
