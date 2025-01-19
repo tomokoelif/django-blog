4a86b7a03f5a52990c8629271a353c4e9920e7f4
Tomoko Koide <tomokoelif@gmail.com>
2025-01-19 14:06:41 +0000

    Refactor imports and enhance forms for collaboration and comments; add tests for form validation and view rendering

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
-
+from .models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status', 'created_on', 'excerpt')
+    list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on',)
     prepopulated_fields = {'slug': ('title',)}
